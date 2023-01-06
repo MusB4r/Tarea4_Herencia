b6f6b308075b995c942fd942c6a51d75ac565c34
MusB4r <devmusbar@gmail.com>
2023-01-06 14:43:01 -0500

    acortar codigo

--- src/Main.java
+++ src/Main.java
@@ -1,66 +1,38 @@
 public class Main {
+
     public static void main(String[] args) {
-        Cliente cliente = new Cliente(30, "Juan", "555-555-5555", 1000.0);
-        System.out.println("Edad: " + cliente.getEdad());
-        System.out.println("Nombre: " + cliente.getNombre());
-        System.out.println("Teléfono: " + cliente.getTelefono());
-        System.out.println("Crédito: " + cliente.getCredito());
+        Cliente cliente = new Cliente();
+        Trabajador trabajador = new Trabajador();
+        cliente.nombre = "Jose";
+        cliente.edad = 23;
+        cliente.telefono = 444-444-444;
+        cliente.credito = 12000;
+        System.out.println("Edad: " + cliente.edad);
+        System.out.println("Nombre: " + cliente.nombre);
+        System.out.println("Teléfono: " + cliente.telefono);
+        System.out.println("Crédito: " + cliente.credito + "\n---------------------");  
+        trabajador.nombre = "Fernando";
+        trabajador.edad = 24;
+        trabajador.telefono = 1111-1111-1111;
+        trabajador.salario = 4600.12;
+        System.out.println("Edad: " + trabajador.edad);
+        System.out.println("Nombre: " + trabajador.nombre);
+        System.out.println("Teléfono: " + trabajador.telefono);
+        System.out.println("Crédito: " + trabajador.salario);
 
-        Trabajador trabajador = new Trabajador(35, "Ana", "444-444-4444", 2000.0);
-        System.out.println("Edad: " + trabajador.getEdad());
-        System.out.println("Nombre: " + trabajador.getNombre());
-        System.out.println("Teléfono: " + trabajador.getTelefono());
-        System.out.println("Salario: " + trabajador.getSalario());
     }
 }
 
-
-class Persona {
-    private int edad;
-    private String nombre;
-    private String telefono;
-
-    public Persona(int edad, String nombre, String telefono) {
-        this.edad = edad;
-        this.nombre = nombre;
-        this.telefono = telefono;
-    }
-
-    public int getEdad() {
-        return edad;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
+class Persona{
+    int edad;
+    int telefono;
+    String nombre;
 }
 
-class Cliente extends Persona {
-    private double credito;
-
-    public Cliente(int edad, String nombre, String telefono, double credito) {
-        super(edad, nombre, telefono);
-        this.credito = credito;
-    }
-
-    public double getCredito() {
-        return credito;
-    }
+class Cliente extends Persona{
+    double credito;
 }
 
 class Trabajador extends Persona {
-    private double salario;
-
-    public Trabajador(int edad, String nombre, String telefono, double salario) {
-        super(edad, nombre, telefono);
-        this.salario = salario;
-    }
-
-    public double getSalario() {
-        return salario;
-    }
+    double salario;
 }
