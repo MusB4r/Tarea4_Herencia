2e285ee7dcbcb4d4251fc5aea7b6b3d026ab401f
MusB4r <devmusbar@gmail.com>
2023-01-06 14:22:39 -0500

    se suve el proyecto

---
+++ src/Main.java
@@ -0,0 +1,66 @@
+public class Main {
+    public static void main(String[] args) {
+        Cliente cliente = new Cliente(30, "Juan", "555-555-5555", 1000.0);
+        System.out.println("Edad: " + cliente.getEdad());
+        System.out.println("Nombre: " + cliente.getNombre());
+        System.out.println("Teléfono: " + cliente.getTelefono());
+        System.out.println("Crédito: " + cliente.getCredito());
+
+        Trabajador trabajador = new Trabajador(35, "Ana", "444-444-4444", 2000.0);
+        System.out.println("Edad: " + trabajador.getEdad());
+        System.out.println("Nombre: " + trabajador.getNombre());
+        System.out.println("Teléfono: " + trabajador.getTelefono());
+        System.out.println("Salario: " + trabajador.getSalario());
+    }
+}
+
+
+class Persona {
+    private int edad;
+    private String nombre;
+    private String telefono;
+
+    public Persona(int edad, String nombre, String telefono) {
+        this.edad = edad;
+        this.nombre = nombre;
+        this.telefono = telefono;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+}
+
+class Cliente extends Persona {
+    private double credito;
+
+    public Cliente(int edad, String nombre, String telefono, double credito) {
+        super(edad, nombre, telefono);
+        this.credito = credito;
+    }
+
+    public double getCredito() {
+        return credito;
+    }
+}
+
+class Trabajador extends Persona {
+    private double salario;
+
+    public Trabajador(int edad, String nombre, String telefono, double salario) {
+        super(edad, nombre, telefono);
+        this.salario = salario;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+}
